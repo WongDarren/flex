89a0e0a261efc282f4ee54888764c0efb458f735
WongDarren <wongdarren1997@gmail.com>
2020-12-08 15:00:37 -0800

    Implement front end log in and register forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => (
-	<Fragment>
-		<Navbar />
-		<Landing />
-	</Fragment>
+	<Router>
+		<Fragment>
+			<Navbar />
+			<Route exact path='/' component={Landing} />
+			<Switch>
+				<Route exact path='/register' component={Register} />
+				<Route exact path='/login' component={Login} />
+			</Switch>
+		</Fragment>
+	</Router>
 );
 
 export default App;
